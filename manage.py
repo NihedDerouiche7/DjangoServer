80860970c1b0287be135d36450c9de485e0f1c1f
Nihed Derouiche <deroucihenihed@gmail.com>
2022-11-29 23:51:08 +0100

    ver_final

--- manage.py
+++ manage.py
@@ -22,7 +22,7 @@ import sys
 #     main()
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Backend.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ProjetTuto.settings")
 
     try:
         from django.core.management import execute_from_command_line
